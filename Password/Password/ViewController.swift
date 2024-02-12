48bb0e544f354bb58af63370bdde295807049926
Figo Lehman <96614364+Figolehman@users.noreply.github.com>
2024-02-12 10:50:06 +0700

    feat: create new password project

---
+++ Password/Password/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Password
+//
+//  Created by Figo Alessandro Lehman on 08/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
